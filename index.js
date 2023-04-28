c08b6889632a0ae9d24522453c3db7562a81101f
mr-exception <pitronum@gmail.com>
2023-04-28 14:53:22 +0300

    added env

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const dotenv = require("dotenv");
 const { Database } = require("sqlite3");
+dotenv.config();
 
-const db = new Database("./x-ui.db");
+const db = new Database(process.env.dbPath);
+db.each("SELECT * FROM users", (err, row) => {
+  console.log(row);
+});
